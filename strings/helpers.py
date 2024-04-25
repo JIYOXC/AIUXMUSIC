5a3db7ffeeb5a2d015a9941ddede2947f0aebd66
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-04-25 21:59:25 +0800

    Add files via upload

---
+++ strings/helpers.py
@@ -0,0 +1,162 @@
+HELP_1 = """✅**<u>Admin Commands:</u>**
+
+**c** stands for channel play.
+
+/pause or /cpause - Pause the playing music.
+/resume or /cresume- Resume the paused music.
+/mute or /cmute- Mute the playing music.
+/unmute or /cunmute- Unmute the muted music.
+/skip or /cskip- Skip the current playing music.
+/stop or /cstop- Stop the playing music.
+/shuffle or /cshuffle- Randomly shuffles the queued playlist.
+/seek or /cseek - Forward Seek the music to your duration
+/seekback or /cseekback - Backward Seek the music to your duration
+/restart - Restart bot for your chat .
+
+✅<u>**Specific Skip:**</u>
+/skip or /cskip [Number(example: 3)] 
+    - Skips music to a the specified queued number. Example: /skip 3 will skip music to third queued music and will ignore 1 and 2 music in queue.
+"""
+
+
+HELP_2 = """✅<u>**Play Commands:**</u>
+
+Available Commands = play , vplay , cplay
+ForcePlay Commands = playforce , vplayforce , cplayforce
+
+**c** stands for channel play.
+**v** stands for video play.
+**force** stands for force play.
+
+/play or /vplay or /cplay  - Bot will start playing your given query on voice chat or Stream live links on voice chats.
+/playforce or /vplayforce or /cplayforce -  **Force Play** stops the current playing track on voice chat and starts playing the searched track instantly without disturbing/clearing queue.
+/channelplay [Chat username or id] or [Disable] - Connect channel to a group and stream music on channel's voice chat from your group.
+
+
+✅**<u>Bot's Server Playlists:</u>**
+/playlist  - Check Your Saved Playlist On Servers.
+/deleteplaylist - Delete any saved music in your playlist
+/play  - Start playing Your Saved Playlist from Servers."""
+
+
+HELP_3 = """✅<u>**Bot Commands:**</u>
+
+/stats - Get Top 10 Tracks Global Stats, Top 10 Users of bot, Top 10 Chats on bot, Top 10 Played in a chat etc etc.
+
+/sudolist - Check Sudo Users of Nezuko Music Bot
+
+/lyrics [Music Name] - Searches Lyrics for the particular Music on web.
+
+/song [Track Name] or [YT Link] - Download any track from youtube in mp3 or mp4 formats.
+
+/player -  Get a interactive Playing Panel.
+
+**c** stands for channel play.
+
+/queue or /cqueue- Check Queue List of Music."""
+
+HELP_4 = """✅<u>**Extra  Commands:**</u>
+/start - Start the Music Bot.
+/help  - Get Commands Helper Menu with detailed explanations of commands.
+/ping- Ping the Bot and check Ram, Cpu etc stats of Bot.
+
+✅<u>**Group Settings:**</u>
+/settings - Get a complete group's settings with inline buttons
+
+
+/playmode - Get a complete play settings panel with buttons where you can set your group's play settings. 
+"""
+
+HELP_5 = """🔰**<u>Sudo Commands :</u>**
+/addsudo [Username or Reply to a user]
+/delsudo [Username or Reply to a user]
+
+📈**<u>STATS COMMANDS:</u>**
+/activevoice - Check active voice chats on bot.
+/activevideo - Check active video calls on bot.
+/stats - Check Bots Stats
+
+⚠️**<u>BLACKLIST CHAT FUNCTION:</u>**
+/blacklistchat [CHAT_ID] - Blacklist any chat from using Music Bot
+/whitelistchat [CHAT_ID] - Whitelist any blacklisted chat from using Music Bot
+/blacklistedchat - Check all blacklisted chats.
+
+
+👤**<u>GBAN FUNCTION:</u>**
+/gban [Username or Reply to a user] - Gban a user from bot's served chat and stop him from using your bot.
+/ungban [Username or Reply to a user] - Remove a user from Bot's gbanned List and allow him for using your bot
+/gbannedusers - Check Gbanned Users Lists
+
+🌐**<u>BROADCAST FUNCTION:</u>**
+/broadcast [Message or Reply to a Message] - Broadcast any message to Bot's Served Chats.
+
+<u>options for broadcast:</u>
+**-pin** | **-pinloud** | **-user** |
+**-assistant** | **-nobot**
+
+**Example:** `/broadcast -user -assistant -pin Hello Testing`
+
+"""
+
+HELP_6 = """
+<b><u>Black Pink :</b></u>
+
+ ⦿ /blackpink - Generates A Blackpink-Style Logo.
+ """
+
+HELP_7 = """
+<b><u>Github :</b></u>
+
+ ⦿ /git [Username] - Get Information About A Github User.
+ ⦿ /github [Username] - Get Information About A Github User.
+ """
+
+HELP_8 = """
+<b><u>Info :</b></u>
+
+/id : Get The Current Group id. If Used By Replying To A Message, Get That User's ID.
+/info : Get Information About A User.
+"""
+
+HELP_9 = """
+<b><u>Telegraph :</b></u>
+
+⦿ /tgm : Uploads A Photo (Under 5ᴍʙ) To The Cloud And Gives A Link.
+"""
+
+HELP_10 = """
+<b><u>Welcome :</b></u>
+
+ ⦿ /welcome [ᴏɴ/ᴏғғ] : Special Welcome On In Your Group.
+"""
+
+
+HELP_11 = """
+<b><u>AFK :</b></u>
+
+⦿ /afk : To Start AFK.
+"""
+
+HELP_12 = """
+<b><u>Couples :</b></u>
+
+⦿ /couples : Get Todays Couples Of The Group In Interactive View.
+"""
+
+HELP_13 = """
+<b><u>Carbon :</b></u>
+
+⦿ /carbon : Make a Carbon.
+"""
+
+HELP_14 = """
+<b><u>Quotly :</b></u>
+
+⦿ /q : Generate Quotly From Message.
+"""
+
+HELP_15 = """
+<b><u>SangMata :</b></u>
+
+⦿ /sg : To Check History Name.
+"""
